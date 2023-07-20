baf445f8b47b6786e6fa6e2d20f9f048f9d89ab8
RyanMYang <ryan.m.yang@gmail.com>
2023-07-19 19:35:09 -0700

    added multirun gpt calls

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ import io
 import datetime
 app = Flask(__name__)
 
-# Load API Key
-load_dotenv()
-openai.api_key = os.getenv('API_KEY')
+log_content = ""
 
 # Input
 def read_ref(file):
@@ -22,87 +20,84 @@ def read_message(file):
         output = f.read()
     return output
 
-## CONTROLS ##
-chat_on = True
-transcript = read_ref("01_transcript")
-system_instructions = read_message("01_system_topics")
-
-
-### Chat GPT
-response = "test"
-
-if chat_on:
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo-16k",  
-    messages=[
-        {"role": "system", "content": system_instructions},
-        {"role": "user", "content": transcript}
-    ]
-    )
-
-# response = openai.ChatCompletion.create(
-#   model="gpt-3.5-turbo",  
-#   messages=[
-#     {"role": "system", "content": "You are an expert in summarizing earnings call transcripts for use by businesses. you provide a summary of parts. 1. An overview in concise paragraph form with the most important, impactful information, explained in a way that emphasizes "},
-#     {"role": "user", "content": "Who won the world cup in 2019?"},
-#     {"role": "assistant", "content": "England cricket team won the world cup in 2019."},
-#     {"role": "user", "content": "Who was the opponent?"}
-#   ]
-# )
-
 
 
 # Output
 
 # Write to
-def write_log(text):
-    filename = datetime.datetime.now().strftime("%m-%d-%H-%M-%S")
+def write_gpt_log(filename, text, instructions, transcript):
     filepath = os.path.join("logs", filename)
     with open(filepath, "w") as f:
         f.write(text)
         f.write("\n\n\n\n\n\n-------INSTRUCTIONS--------")
-        f.write(system_instructions)
+        f.write(instructions)
         f.write("\n\n\n\n\n\n-------TRANSCRIPT--------")
         f.write(transcript)
     return filename
 
-# Write to log
-if (type(response) is str):
-    log = write_log(response)
-else:
-    log = write_log(response["choices"][0]["message"]["content"])
+
+def gpt35_16k(sys, usr):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",  
+        messages=[
+            {"role": "system", "content": sys},
+            {"role": "user", "content": usr}
+        ]
+        )
+    return completion
+
+def gpt35(sys, usr):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",  
+        messages=[
+            {"role": "system", "content": sys},
+            {"role": "user", "content": usr}
+        ]
+        )
+    return completion
+
+def main():
+    # Load API Key
+    load_dotenv()
+    openai.api_key = os.getenv('API_KEY')
+
+    ## CONTROLS ##
+    chat_on = False
+    summary_count = 2
+    transcript = read_ref("01_transcript")
+    system_instructions = read_message("01_system_topics_no_categories")
 
 
-print(response)
-log_content=""
-with open(os.path.join("logs", log), "r") as f:
-    log_content = f.read()
+    log_time = datetime.datetime.now().strftime("%m-%d-%H-%M-%S")
+    if (transcript == None or system_instructions == None):
+        print("Input file error")
+        quit()
 
-def get_file_list(directory):
-    """Returns a list of all the files in the directory."""
-    file_list = []
-    for file in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, file)):
-            file_list.append(file)
-    return file_list
+    ### Chat GPT
+    responses = list()
 
-def create_menu(file_list):
-    """Creates a menu with a link for each file in the list."""
-    menu = ""
-    for file in file_list:
-        menu += f"<a href='{file}'>{file}</a>"
-    return menu
-
-def redirect_to_file(file):
-    """Redirects the user to the file's contents."""
-    return f"/file/{file}"
+    if chat_on:
+        for i in range(summary_count):
+            responses.append(gpt35_16k(system_instructions, transcript))
+            log = write_gpt_log(log_time+"_"+str(i),responses[i]["choices"][0]["message"]["content"])
+    else:
+        for i in range(summary_count):
+            responses.append("Summary Test" + str(i))
+            log = write_gpt_log(log_time+"_"+str(i),responses[i], system_instructions, transcript)
+    
+        
+    print(responses)
+    # with open(os.path.join("logs", log), "r") as f:
+    #     log_content = f.read()
 
 @app.route('/')
 def index():
-    return log_content
+    return "Testing"
     # file_list = get_file_list('logs')
     # menu = create_menu(file_list)
     # return render_template('menu.html', menu=menu)
 
 if __name__ == '__main__':
+    main()
     app.run()
+
