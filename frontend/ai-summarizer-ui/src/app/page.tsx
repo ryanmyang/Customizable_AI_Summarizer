c1d93bc98f55161c02d1ae6e0ff601e904e0b6e2
RyanMYang <ryan.m.yang@gmail.com>
2023-09-27 09:48:49 -0700

    basically no changes, need to test build with env vars

--- frontend/ai-summarizer-ui/src/app/page.tsx
+++ frontend/ai-summarizer-ui/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <CallMainButton/>
-        <FileDropdown/>
+        {/* <FileDropdown/> */}
       </div>
     </main>
   )
